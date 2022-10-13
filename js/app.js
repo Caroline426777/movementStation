960aee4c594c428f3a6206b3ef48deff8f12934d
426777 <426777@student.fontys.nl>
2022-10-13 12:41:52 +0200

    concrete mask

--- js/app.js
+++ js/app.js
@@ -43,23 +43,17 @@ function draw() {
   // These graphics are placeholders/ code examples- not designs.
   // Feel free to change or remove them as you wish.
   for (let i = 0; i < data.output.persons.length; i++) {
-    generateRadius();
+   // generateRadius();
     drawConcrete();
-    //drawInsulation();
-    //drawBrick();
-    //drawTarp();
   }
 }
 
 //Generates the layers around the last texture
-function generateRadius() {
-  for (var a = blobAmount; a > 0; a--) {
-     drawConcreteBkgr(a);
-    //drawInsulationBkgr(a);
-    //drawBrickBkgr(a);
-    //drawTarpBkgr(a);
-  }
-}
+// function generateRadius() {
+//   for (var a = blobAmount; a > 0; a--) {
+//      drawConcreteBkgr(a);
+//   }
+// }
 
 
 //Concrete texture layers
@@ -67,26 +61,28 @@ function drawConcrete(){
   push ();
   translate(
     data.output.persons[0].centerPoint["x"],
-    data.output.persons[0].centerPoint["y"]
+    data.output.persons[0].centerPoint["y"], 200
   );
 
-  let shapeMask = createGraphics(400, 400);
-  shapeMask.fill('rgba(0, 0, 0, 1)');
-  //shapeMask.ellipse(200, 200, 200, 200);
-  shapeMask.beginShape();
-  var radius = 150;
-  for (let i = 0; i < TWO_PI; i += 0.1) {
-      var offset = shapeMask.map(noise(i, millis() / 1000), 0, 1, -60, 60);
-      var r = radius + offset;
-      var x = cos(i) * r;
-      var y = sin(i) * r;
-      shapeMask.vertex(x, y * 1.7);
+  concreteMask.clear();
+
+  concreteMask.fill('rgba(0, 0, 0, 1)');
+
+  concreteMask.beginShape();
+  concreteClone = concrete.get(); 
+  for(var a = 0; a < TWO_PI; a += 0.1){
+    var offset = map(noise(a, millis() / 1000), -1, 1, -20, 20);
+    var r = 50 + offset;
+    var x = cos(a) * r;
+    var y = sin(a) * -r;
+    vertex(x + 70, y + 70);
+
   }
+  concreteMask.endShape();
+  concreteClone.mask(concreteMask);
 
-  shapeMask.endShape(CLOSE);
+  image(concreteClone, -200, -200, 800, 600);
 
-  //concrete.mask(shapeMask);
-  image(shapeMask, -200, -280);
 
   pop ();
 }
