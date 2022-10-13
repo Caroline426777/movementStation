999779336e19db55544582e4162691e7dbf84d4c
426777 <426777@student.fontys.nl>
2022-10-13 12:36:34 +0200

    Adding mask textures

--- js/app.js
+++ js/app.js
@@ -5,25 +5,31 @@ let windowH = window.innerHeight;
 let blobRadius = 50;
 let blobAmount = 14;
 
-let yoff = 0.0;
+//Variables for the images
 let concrete;
-let insulation;
 let brick;
-let tarp;
 
+//Variables for the masks 
+let concreteMask;
+let brickMask;
 
-function preload() {
-  concrete = loadImage("assets/concrete.jpg");
-  insulation = loadImage("assets/insulation.jpg");
-  brick = loadImage("assets/brick.jpg");
-  tarp = loadImage("assets/tarp.jpg");
-}
+//Variables for the cloning of the images
+let concreteClone;
+let brickClone;
+
 
 function setup() {
   pixelDensity(1);
   data = new Data();
   createCanvas(windowW, windowH, P2D);
-  noStroke();
+
+  //Graphics that create the mask
+  concreteMask = createGraphics(300, 150);
+  brickMask = createGraphics(300, 200);
+  
+  //Loading in the images
+  concrete = loadImage("assets/concrete.jpg");
+  brick = loadImage("assets/brick.jpg");
 }
 
 function draw() {
@@ -73,113 +79,20 @@ function drawConcrete(){
       var offset = shapeMask.map(noise(i, millis() / 1000), 0, 1, -60, 60);
       var r = radius + offset;
       var x = cos(i) * r;
-      var y = sin(i) * -r;
+      var y = sin(i) * r;
       shapeMask.vertex(x, y * 1.7);
-    }
+  }
 
   shapeMask.endShape(CLOSE);
 
-  concrete.mask(shapeMask);
-  image(concrete, -200, -280, 400, 500);
-
-  
-
-  // beginShape();
-  // fill(181, 181, 181, 255);
-  // // Exterior part of shape, clockwise winding
-  // vertex(-200, -300);
-  // vertex(200, -300);
-  // vertex(200, 300);
-  // vertex(-200, 300);
-  // // Interior part of shape, counter-clockwise winding
-  // beginContour();
-
-  // var radius = 150;
-
-  // for (let i = 0; i < TWO_PI; i += 0.1) {
-  //   var offset = map(noise(i, millis() / 1000), 0, 1, -60, 60);
-  //   var r = radius + offset;
-  //   var x = cos(i) * r;
-  //   var y = sin(i) * -r;
-  //   vertex(x, y * 1.7);
-  // }
-
-  // endContour();
-  // endShape(CLOSE);
+  //concrete.mask(shapeMask);
+  image(shapeMask, -200, -280);
 
   pop ();
 }
 
 //Generates the concrete background layers
-function drawConcreteBkgr(a) {
-  // push();
-  // translate(
-  //   data.output.persons[0].centerPoint["x"],
-  //   data.output.persons[0].centerPoint["y"],
-  //   200
-  // );
-  // var radius = blobRadius * a;
-
-  // fill(181, 181, 181, 255 - (255 / blobAmount) * a);
-
-  // noStroke();
-  // //texture (img);
-  // beginShape();
-
-  // for (let a = 0; a < TWO_PI; a += 0.1) {
-  //   var offset = map(noise(a, millis() / 1000), 0, 1, -60, 60);
-  //   var r = radius + offset;
-  //   var x = cos(a) * r;
-  //   var y = sin(a) * -r;
-  //   vertex(x, y * 1.7);
-  //   //ellipse(x, y, 4, 4);
-  // }
-  // endShape();
-  // pop();
-
-  // yoff += 0.008;
-}
-
-
-//Insulation texture layers
-function drawInsulation(){
-  push ();
-  translate(
-    data.output.persons[0].centerPoint["x"],
-    data.output.persons[0].centerPoint["y"],
-    200
-  );
-
-  image(insulation, -200, -280, 400, 580);
-
-  beginShape();
-  fill(250, 175, 165);
-  // Exterior part of shape, clockwise winding
-  vertex(-200, -300);
-  vertex(200, -300);
-  vertex(200, 300);
-  vertex(-200, 300);
-  // Interior part of shape, counter-clockwise winding
-  beginContour();
-
-  var radius = 150;
-
-  for (let i = 0; i < TWO_PI; i += 0.1) {
-    var offset = map(noise(i, millis() / 1000), 0, 1, -25, 25);
-    var r = radius + offset;
-    var x = cos(i) * r;
-    var y = sin(i) * -r;
-    vertex(x, y * 1.7);
-  }
-
-  endContour();
-  endShape(CLOSE);
-
-  pop ();
-}
-
-//Generates the insulation texture layers
-function drawInsulationBkgr(a) {
+/*function drawConcreteBkgr(a) {
   push();
   translate(
     data.output.persons[0].centerPoint["x"],
@@ -188,14 +101,14 @@ function drawInsulationBkgr(a) {
   );
   var radius = blobRadius * a;
 
-  fill(250, 175, 165, 255 - (255 / blobAmount) * a);
+  fill(181, 181, 181, 255 - (255 / blobAmount) * a);
 
   noStroke();
   //texture (img);
   beginShape();
 
   for (let a = 0; a < TWO_PI; a += 0.1) {
-    var offset = map(noise(a, millis() / 1000), 0, 1, -25, 25);
+    var offset = map(noise(a, millis() / 1000), 0, 1, -60, 60);
     var r = radius + offset;
     var x = cos(a) * r;
     var y = sin(a) * -r;
@@ -205,137 +118,4 @@ function drawInsulationBkgr(a) {
   endShape();
   pop();
 
-  yoff += 0.008;
-}
-
-//Brick texture layers
-function drawBrick(){
-  push ();
-  translate(
-    data.output.persons[0].centerPoint["x"],
-    data.output.persons[0].centerPoint["y"],
-    200
-  );
-
-  image(brick, -200, -280, 400, 580);
-
-  beginShape();
-  fill(203, 86, 52);
-  // Exterior part of shape, clockwise winding
-  vertex(-200, -300);
-  vertex(200, -300);
-  vertex(200, 300);
-  vertex(-200, 300);
-  // Interior part of shape, counter-clockwise winding
-  beginContour();
-
-  var radius = 150;
-
-  for (let i = 0; i < TWO_PI; i += 0.1) {
-    var offset = map(noise(i, millis() / 1000), 0, 1, -100, 100);
-    var r = radius + offset;
-    var x = cos(i) * r;
-    var y = sin(i) * -r;
-    vertex(x, y * 1.7);
-  }
-
-  endContour();
-  endShape(CLOSE);
-
-  pop ();
-}
-
-//Generates the brick texture layers
-function drawBrickBkgr(a) {
-  push();
-  translate(
-    data.output.persons[0].centerPoint["x"],
-    data.output.persons[0].centerPoint["y"],
-    200
-  );
-  var radius = blobRadius * a;
-
-  fill(203, 86, 52, 255 - (255 / blobAmount) * a);
-
-  noStroke();
-  //texture (img);
-  beginShape();
-
-  for (let a = 0; a < TWO_PI; a += 0.1) {
-    var offset = map(noise(a, millis() / 1000), 0, 1, -100, 100);
-    var r = radius + offset;
-    var x = cos(a) * r;
-    var y = sin(a) * -r;
-    vertex(x, y * 1.7);
-    //ellipse(x, y, 4, 4);
-  }
-  endShape();
-  pop();
-
-  yoff += 0.008;
-}
-
-//Brick texture layers
-function drawTarp(){
-  push ();
-  translate(
-    data.output.persons[0].centerPoint["x"],
-    data.output.persons[0].centerPoint["y"],
-    200
-  );
-
-  image(tarp, -200, -280, 400, 580);
-
-  beginShape();
-  fill(45, 136, 195);
-  // Exterior part of shape, clockwise winding
-  vertex(-200, -300);
-  vertex(200, -300);
-  vertex(200, 300);
-  vertex(-200, 300);
-  // Interior part of shape, counter-clockwise winding
-  beginContour();
-
-  var radius = 150;
-
-  for (let i = 0; i < TWO_PI; i += 0.1) {
-    var offset = map(noise(i, millis() / 600), 0, 1, -20, 20);
-    var r = radius + offset;
-    var x = cos(i) * r;
-    var y = sin(i) * -r;
-    vertex(x * 1.2, y * 1.5);
-  }
-
-  endContour();
-  endShape(CLOSE);
-
-  pop ();
-}
-
-//Generates the tarp texture layers
-function drawTarpBkgr(a) {
-  push();
-  translate(
-    data.output.persons[0].centerPoint["x"],
-    data.output.persons[0].centerPoint["y"],
-    200
-  );
-  var radius = blobRadius * a;
-
-  fill(45, 136, 195, 255 - (255 / blobAmount) * a);
-
-  noStroke();
-  //texture (img);
-  beginShape();
-
-  for (let a = 0; a < TWO_PI; a += 0.1) {
-    var offset = map(noise(a, millis() / 600), 0, 1, -20, 20);
-    var r = radius + offset;
-    var x = cos(a) * r;
-    var y = sin(a) * -r;
-    vertex(x * 1.2, y * 1.5);
-    //ellipse(x, y, 4, 4);
-  }
-  endShape();
-  pop();
-}
+}*/
