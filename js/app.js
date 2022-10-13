0c95dd6f477fe9382dca9083a28fe59f440ec952
426777 <426777@student.fontys.nl>
2022-10-13 14:30:12 +0200

    Making code smaller

--- js/app.js
+++ js/app.js
@@ -18,9 +18,8 @@ let brickMask;
 let brickTwoMask;
 
 //Variables for the cloning of the images
-let concreteClone;
-let brickClone;
-let brickTwoClone;
+let textureClone;
+
 
 
 function setup() {
@@ -28,15 +27,35 @@ function setup() {
   data = new Data();
   createCanvas(windowW, windowH, P2D);
 
-  //Graphics that create the mask
-  concreteMask = createGraphics(300, 142);
-  brickMask = createGraphics(350, 190);
-  brickTwoMask = createGraphics(450, 350);
+ concrete = {
+    mask: createGraphics(300, 142),
+    image: loadImage("assets/concrete.jpg"),
+    customLocationX:70,
+    customLocationY:70,
+    rangeStart: -30,
+    rangeEnd: 30
+  }
+
+  brick = {
+    mask: createGraphics(350, 190),
+    image: loadImage("assets/brick.jpg"),
+    customLocationX:75,
+    customLocationY:70,
+    rangeStart: -20,
+    rangeEnd: 20
+  }
+
+  brickTwo = {
+    mask: createGraphics(450, 350),
+    image: loadImage("assets/brick2.jpg"),
+    customLocationX:85,
+    customLocationY:75,
+    rangeStart: -20,
+    rangeEnd: 20
+  }
   
-  //Loading in the images
-  concrete = loadImage("assets/concrete.jpg");
-  brick = loadImage("assets/brick.jpg");
-  brickTwo = loadImage("assets/brick2.jpg");
+
+  textures = [concrete, brick, brickTwo];
 }
 
 function draw() {
@@ -51,7 +70,11 @@ function draw() {
   // Feel free to change or remove them as you wish.
   for (let i = 0; i < data.output.persons.length; i++) {
     generateRadius();
-    drawConcrete(i);
+
+    for(let s = 0; s < textures.length; s++){
+      texture = textures[s];
+      generateShape(texture, i);
+    }
   }
 }
 
@@ -62,80 +85,110 @@ function generateRadius() {
   }
 }
 
-
-//Concrete texture layers
-function drawConcrete(i){
-  push ();
+function generateShape(texture, i){
+  push();
   translate(
-    data.output.persons[i].centerPoint["x"],
-    data.output.persons[i].centerPoint["y"], 200
+  data.output.persons[i].centerPoint["x"],
+  data.output.persons[i].centerPoint["y"], 200
   );
 
-  var xoff = 0;
+  xoff = 0;
+  texture.mask.clear();
 
-  concreteMask.clear();
+  texture.mask.fill('rgba(0, 0, 0, 1)');
 
-  concreteMask.fill('rgba(0, 0, 0, 1)');
-
-  concreteMask.beginShape();
-  concreteClone = concrete.get(); 
+  texture.mask.beginShape();
+  textureClone = texture.image.get(); 
   for(var a = 0; a < TWO_PI; a += 0.1){
-    var offset = map(noise(xoff, yoff), -1, 1, -30, 30);
+    var offset = map(noise(xoff, yoff), -1, 1, texture.rangeStart, texture.rangeEnd);
     var r = 50 + offset;
     var x = cos(a) * r;
     var y = sin(a) * -r;
-    vertex(x + 70, y + 70);
+    vertex(x + texture.customLocationX, y + texture.customLocationY);
     xoff += 0.1;
   }
-  concreteMask.endShape();
-  concreteClone.mask(concreteMask);
+  texture.mask.endShape();
+  textureClone.mask(texture.mask);
 
-  image(concreteClone, -200, -200, 800, 700);
-
-  //BRICK 1 TEXTURE
-  brickMask.clear();
-
-  brickMask.fill('rgba(0, 0, 0, 1)');
-
-  brickMask.beginShape();
-  brickClone = brick.get(); 
-  for(var a = 0; a < TWO_PI; a += 0.1){
-    var offset = map(noise(xoff, yoff), -1, 1, -20, 20);
-    var r = 50 + offset;
-    var x = cos(a) * r;
-    var y = sin(a) * -r;
-    vertex(x + 75, y + 70);
-    xoff += 0.2;
-  }
-  brickMask.endShape();
-  brickClone.mask(brickMask);
-
-  image(brickClone, -200, -200, 800, 700);
-
-
-  //BRICK 2 TEXTURE
-  brickTwoMask.clear();
-
-  brickTwoMask.fill('rgba(0, 0, 0, 1)');
-
-  brickTwoMask.beginShape();
-  brickTwoClone = brickTwo.get(); 
-  for(var a = 0; a < TWO_PI; a += 0.1){
-    var offset = map(noise(xoff, yoff), -1, 1, -20, 20);
-    var r = 50 + offset;
-    var x = cos(a) * r;
-    var y = sin(a) * -r;
-    vertex(x + 85, y + 75);
-    xoff += 0.2;
-  }
-  brickTwoMask.endShape();
-  brickTwoClone.mask(brickTwoMask);
-
-  image(brickTwoClone, -200, -200, 800, 700);
+  image(textureClone, -200, -200, 800, 700);
   pop ();
-
   yoff += 0.05;
 }
+
+
+//Concrete texture layers
+// function drawConcrete(i){
+//   push ();
+//   translate(
+//     data.output.persons[i].centerPoint["x"],
+//     data.output.persons[i].centerPoint["y"], 200
+//   );
+
+//   var xoff = 0;
+
+//   concreteMask.clear();
+
+//   concreteMask.fill('rgba(0, 0, 0, 1)');
+
+//   concreteMask.beginShape();
+//   concreteClone = concrete.get(); 
+//   for(var a = 0; a < TWO_PI; a += 0.1){
+//     var offset = map(noise(xoff, yoff), -1, 1, -30, 30);
+//     var r = 50 + offset;
+//     var x = cos(a) * r;
+//     var y = sin(a) * -r;
+//     vertex(x + 70, y + 70);
+//     xoff += 0.1;
+//   }
+//   concreteMask.endShape();
+//   concreteClone.mask(concreteMask);
+
+//   image(concreteClone, -200, -200, 800, 700);
+
+//   //BRICK 1 TEXTURE
+//   brickMask.clear();
+
+//   brickMask.fill('rgba(0, 0, 0, 1)');
+
+//   brickMask.beginShape();
+//   brickClone = brick.get(); 
+//   for(var a = 0; a < TWO_PI; a += 0.1){
+//     var offset = map(noise(xoff, yoff), -1, 1, -20, 20);
+//     var r = 50 + offset;
+//     var x = cos(a) * r;
+//     var y = sin(a) * -r;
+//     vertex(x + 75, y + 70);
+//     xoff += 0.2;
+//   }
+//   brickMask.endShape();
+//   brickClone.mask(brickMask);
+
+//   image(brickClone, -200, -200, 800, 700);
+
+
+//   //BRICK 2 TEXTURE
+//   brickTwoMask.clear();
+
+//   brickTwoMask.fill('rgba(0, 0, 0, 1)');
+
+//   brickTwoMask.beginShape();
+//   brickTwoClone = brickTwo.get(); 
+//   for(var a = 0; a < TWO_PI; a += 0.1){
+//     var offset = map(noise(xoff, yoff), -1, 1, -20, 20);
+//     var r = 50 + offset;
+//     var x = cos(a) * r;
+//     var y = sin(a) * -r;
+//     vertex(x + 85, y + 75);
+//     xoff += 0.2;
+//   }
+//   brickTwoMask.endShape();
+//   brickTwoClone.mask(brickTwoMask);
+
+//   image(brickTwoClone, -200, -200, 800, 700);
+//   pop ();
+
+//   yoff += 0.05;
+// }
 
 //Generates the concrete background layers
 function drawConcreteBkgr(i) {
