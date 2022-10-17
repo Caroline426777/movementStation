36fb58b67e41981ff7866d686adce135a3aa2e17
426777 <426777@student.fontys.nl>
2022-10-17 09:42:36 +0200

    Adding all blobs

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let windowW = window.innerWidth;
 let windowH = window.innerHeight;
 
 let yoff = 0;
+let yoffG = 0;
 
 let blobRadius = 50;
 let blobAmount = 12;
@@ -17,83 +18,83 @@ function setup() {
   data = new Data();
   createCanvas(windowW, windowH, P2D);
 
-  //RED BRICK AND CONCRETE SHAPE
+  //RED BRICK AND CONCRETE SHAPE -- Sharp edges
   concrete = {
     mask: createGraphics(350, 200),
     image: loadImage("assets/concrete.jpg"),
-    customLocationX: 80,
+    customLocationX: 90,
     customLocationY: 120,
-    rangeStart: -20,
-    rangeEnd: 20,
-    speedNoiseY: 0.05,
-    speedNoiseX: 0.1,
+    rangeStart: -50,
+    rangeEnd: 50,
+    speedNoiseY: 0.001,
+    speedNoiseX: 0.5,
   }
   concreteTwo = {
     mask: createGraphics(250, 200),
     image: loadImage("assets/concrete.jpg"),
-    customLocationX: 70,
-    customLocationY: 70,
-    rangeStart: -20,
-    rangeEnd: 20,
-    speedNoiseY: 0.05,
-    speedNoiseX: 0.1,
+    customLocationX: 90,
+    customLocationY: 80,
+    rangeStart: -50,
+    rangeEnd: 50,
+    speedNoiseY: 0.001,
+    speedNoiseX: 0.5,
   }
 
   brick = {
     mask: createGraphics(350, 170),
     image: loadImage("assets/brick.jpg"),
-    customLocationX: 75,
-    customLocationY: 80,
-    rangeStart: -20,
-    rangeEnd: 20,
-    speedNoiseY: 0.01,
-    speedNoiseX: 0.1,
+    customLocationX: 95,
+    customLocationY: 90,
+    rangeStart: -40,
+    rangeEnd: 40,
+    speedNoiseY: 0.001,
+    speedNoiseX: 0.4,
   }
   brickTwo = {
     mask: createGraphics(270, 250),
     image: loadImage("assets/brick.jpg"),
-    customLocationX: 75,
-    customLocationY: 80,
-    rangeStart: -20,
-    rangeEnd: 20,
-    speedNoiseY: 0.01,
-    speedNoiseX: 0.1,
+    customLocationX: 95,
+    customLocationY: 90,
+    rangeStart: -40,
+    rangeEnd: 40,
+    speedNoiseY: 0.001,
+    speedNoiseX: 0.4,
   }
 
   brickRed = {
     mask: createGraphics(400, 250),
     image: loadImage("assets/brick2.jpg"),
-    customLocationX: 85,
+    customLocationX: 95,
     customLocationY: 85,
-    rangeStart: -20,
-    rangeEnd: 20,
-    speedNoiseY: 0.03,
-    speedNoiseX: 0.1,
+    rangeStart: -30,
+    rangeEnd: 30,
+    speedNoiseY: 0.001,
+    speedNoiseX: 0.3,
   }
 
   brickDark = {
     mask: createGraphics(500, 700),
     image: loadImage("assets/brick3.jpg"),
-    customLocationX: 150,
+    customLocationX: 160,
     customLocationY: 450,
     rangeStart: -20,
     rangeEnd: 20,
-    speedNoiseY: 0.03,
-    speedNoiseX: 0.1,
+    speedNoiseY: 0.001,
+    speedNoiseX: 0.3,
   }
   brickDarkTwo = {
     mask: createGraphics(800, 550),
     image: loadImage("assets/brick3.jpg"),
-    customLocationX: 300,
+    customLocationX: 310,
     customLocationY: 380,
     rangeStart: -20,
     rangeEnd: 20,
-    speedNoiseY: 0.03,
-    speedNoiseX: 0.1,
+    speedNoiseY: 0.001,
+    speedNoiseX: 0.3,
   }
 
 
-  //INSULATION, WOOD, SAND SHAPE
+  //INSULATION, WOOD, SAND SHAPE -- FLOWING
   insulation = {
     mask: createGraphics(250, 160),
     image: loadImage("assets/insulation.jpg"),
@@ -101,28 +102,28 @@ function setup() {
     customLocationY: 70,
     rangeStart: -15,
     rangeEnd: 15,
-    speedNoiseY: 0.001,
+    speedNoiseY: 0.01,
     speedNoiseX: 0.1,
   }
 
   sand = {
-    mask: createGraphics(350, 450),
+    mask: createGraphics(350, 400),
     image: loadImage("assets/sand.jpg"),
     customLocationX: 90,
     customLocationY: 200,
-    rangeStart: -15,
-    rangeEnd: 15,
-    speedNoiseY: 0.001,
+    rangeStart: -20,
+    rangeEnd: 20,
+    speedNoiseY: 0.01,
     speedNoiseX: 0.1,
   }
   sandTwo = {
-    mask: createGraphics(450, 550),
+    mask: createGraphics(400, 550),
     image: loadImage("assets/sand.jpg"),
     customLocationX: 120,
     customLocationY: 150,
-    rangeStart: -15,
-    rangeEnd: 15,
-    speedNoiseY: 0.001,
+    rangeStart: -20,
+    rangeEnd: 20,
+    speedNoiseY: 0.01,
     speedNoiseX: 0.1,
   }
 
@@ -133,7 +134,7 @@ function setup() {
     customLocationY: 150,
     rangeStart: -15,
     rangeEnd: 15,
-    speedNoiseY: 0.001,
+    speedNoiseY: 0.01,
     speedNoiseX: 0.1,
   }
   woodTwo = {
@@ -143,19 +144,19 @@ function setup() {
     customLocationY: 120,
     rangeStart: -15,
     rangeEnd: 15,
-    speedNoiseY: 0.001,
+    speedNoiseY: 0.01,
     speedNoiseX: 0.1,
   }
 
 
-  //PLASTER, TARP, GLASS
+  // //PLASTER, TARP, GLASS -- NOT SHARP BUT BUBBLY
   plaster = {
     mask: createGraphics(230, 250),
     image: loadImage("assets/plaster.jpg"),
     customLocationX: 70,
     customLocationY: 70,
-    rangeStart: -25,
-    rangeEnd: 25,
+    rangeStart: -40,
+    rangeEnd: 40,
     speedNoiseY: 0.01,
     speedNoiseX: 0.1,
   }
@@ -164,8 +165,8 @@ function setup() {
     image: loadImage("assets/plaster.jpg"),
     customLocationX: 135,
     customLocationY: 70,
-    rangeStart: -25,
-    rangeEnd: 25,
+    rangeStart: -40,
+    rangeEnd: 40,
     speedNoiseY: 0.01,
     speedNoiseX: 0.1,
   }
@@ -174,8 +175,8 @@ function setup() {
     image: loadImage("assets/plaster.jpg"),
     customLocationX: 118,
     customLocationY: 345,
-    rangeStart: -25,
-    rangeEnd: 25,
+    rangeStart: -40,
+    rangeEnd: 40,
     speedNoiseY: 0.01,
     speedNoiseX: 0.1,
   }
@@ -197,7 +198,7 @@ function setup() {
     rangeStart: -50,
     rangeEnd: 50,
     speedNoiseY: 0.01,
-    speedNoiseX: 0.3,
+    speedNoiseX: 0.1,
   }
   tarpTwo ={
     mask: createGraphics(500, 500),
@@ -207,7 +208,7 @@ function setup() {
     rangeStart: -50,
     rangeEnd: 50,
     speedNoiseY: 0.01,
-    speedNoiseX: 0.3,
+    speedNoiseX: 0.1,
   }
   glass ={
     mask: createGraphics(250, 350),
@@ -220,22 +221,99 @@ function setup() {
     speedNoiseX: 0.15,
   }
 
+
+  //CARPET, INSULATION, ROUGH CONCRETE -- MEDIUM SHARP
+  carpet ={
+    mask: createGraphics(250, 180),
+    image: loadImage("assets/carpet.jpg"),
+    customLocationX: 70,
+    customLocationY: 100,
+    rangeStart: -30,
+    rangeEnd: 30,
+    speedNoiseY: 0.01,
+    speedNoiseX: 0.4,
+  }
+  carpetTwo ={
+    mask: createGraphics(350, 350),
+    image: loadImage("assets/carpet.jpg"),
+    customLocationX: 100,
+    customLocationY: 70,
+    rangeStart: -30,
+    rangeEnd: 30,
+    speedNoiseY: 0.01,
+    speedNoiseX: 0.4,
+  }
+  blueInsulation ={
+    mask: createGraphics(300, 400),
+    image: loadImage("assets/blue-insulation.jpg"),
+    customLocationX: 80,
+    customLocationY: 110,
+    rangeStart: -20,
+    rangeEnd: 20,
+    speedNoiseY: 0.01,
+    speedNoiseX: 0.3,
+  }
+  blueInsulationTwo ={
+    mask: createGraphics(350, 300),
+    image: loadImage("assets/blue-insulation.jpg"),
+    customLocationX: 100,
+    customLocationY: 150,
+    rangeStart: -20,
+    rangeEnd: 20,
+    speedNoiseY: 0.01,
+    speedNoiseX: 0.3,
+  }
+  brownConcrete ={
+    mask: createGraphics(350, 450),
+    image: loadImage("assets/concrete-brown.jpg"),
+    customLocationX: 100,
+    customLocationY: 300,
+    rangeStart: -20,
+    rangeEnd: 20,
+    speedNoiseY: 0.01,
+    speedNoiseX: 0.4,
+  }
+  brownConcreteTwo ={
+    mask: createGraphics(500, 350),
+    image: loadImage("assets/concrete-brown.jpg"),
+    customLocationX: 110,
+    customLocationY: 220,
+    rangeStart: -20,
+    rangeEnd: 20,
+    speedNoiseY: 0.01,
+    speedNoiseX: 0.4,
+  }
+  brownConcreteThree ={
+    mask: createGraphics(300, 500),
+    image: loadImage("assets/concrete-brown.jpg"),
+    customLocationX: 110,
+    customLocationY: 70,
+    rangeStart: -20,
+    rangeEnd: 20,
+    speedNoiseY: 0.01,
+    speedNoiseX: 0.4,
+  }
+
+
   texturesHard = [concrete, concreteTwo, brick, brickTwo, brickRed, brickDark, brickDarkTwo];
 
   texturesSoft = [insulation, sand, sandTwo, wood, woodTwo];
 
   texturesFluid = [plaster, plasterTwo, plasterThree, plasterFour, tarp, tarpTwo, glass];
 
-  textureBlobs = [texturesHard, texturesSoft, texturesFluid];
+  texturesNeutral = [carpet, carpetTwo, blueInsulation, blueInsulationTwo, brownConcrete, brownConcreteTwo, brownConcreteThree];
+
+  textureBlobs = [texturesHard, texturesSoft, texturesFluid, texturesNeutral];
 
   // BACKGROUND LAYERS 
   grey = {
     color: (181, 181, 181),
-    rangeLayerStart: -60,
-    rangeLayerEnd: 60,
+    rangeLayerStart: -90,
+    rangeLayerEnd: 90,
     locationXWidth: 0,
     locationYHeight: 2,
-    speedLayerNoise: 0.03,
+    speedLayerNoiseY: 0.001,
+    speedLayerNoiseX: 0.5,
   }
 
   pink = {
@@ -244,7 +322,8 @@ function setup() {
     rangeLayerEnd: 20,
     locationXWidth: 1.2,
     locationYHeight: 1.8,
-    speedLayerNoise: 0.01,
+    speedLayerNoiseY: 0.01,
+    speedLayerNoiseX: 0.1,
   }
 
   layers = [grey, pink];
@@ -262,10 +341,10 @@ function draw() {
   // Feel free to change or remove them as you wish.
   for (let i = 0; i < data.output.persons.length; i++) {
 
-    // for (let r = 0; r < layers.length; r++) {
-    //   layer = layers[r];
-    //   generateRadius(layer, i);
-    // }
+    for (let r = 0; r < layers.length; r++) {
+      layer = layers[r];
+      generateRadius(layer, i);
+    }
     for (let h = 0; h < texturesHard.length; h++) {
       texture = texturesHard[h];
       generateShape(texture, i);
@@ -278,8 +357,13 @@ function draw() {
       texture = texturesFluid[f];
       generateShape(texture, i);
     }
+    for (let n = 0; n < texturesNeutral.length; n++) {
+      texture = texturesNeutral[n];
+      generateShape(texture, i);
+    }
 
-    
+    let textureBlob = random(textureBlobs);
+    return textureBlob;
 
   }
 }
@@ -315,7 +399,7 @@ function generateShape(texture, i) {
   texture.mask.endShape();
   textureClone.mask(texture.mask);
 
-  image(textureClone, -200, -250, 800, 800);
+  image(textureClone, -250, -400, 800, 800);
   pop();
   yoff += texture.speedNoiseY;
 }
@@ -334,19 +418,19 @@ function drawConcreteBkgr(i) {
   fill(layer.color, 255 - (255 / blobAmount) * i);
 
   noStroke();
-  xoff = 0;
+  var xoffG = 0;
 
   beginShape();
 
   for (let i = 0; i < TWO_PI; i += 0.1) {
-    var offset = map(noise(xoff, yoff), 0, 1, layer.rangeLayerStart, layer.rangeLayerEnd);
+    var offset = map(noise(xoffG, yoffG), -1, 1, layer.rangeLayerStart, layer.rangeLayerEnd);
     var r = radius + offset;
     var x = cos(i) * r;
     var y = sin(i) * -r;
     vertex(x * layer.locationXWidth, y * layer.locationYHeight);
-    xoff += 0.1;
+    xoffG += layer.speedLayerNoiseX;
   }
   endShape();
   pop();
-  yoff += layer.speedLayerNoise;
+  yoffG += layer.speedLayerNoiseY;
 }
